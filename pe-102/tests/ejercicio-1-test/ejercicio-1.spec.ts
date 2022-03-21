31e8f695978585ab173abaaa0c17cf52102c763b
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-21 09:54:31 +0000

    Test Collectable removeItem() TDD

--- pe-102/tests/ejercicio-1-test/ejercicio-1.spec.ts
+++ pe-102/tests/ejercicio-1-test/ejercicio-1.spec.ts
@@ -11,5 +11,9 @@ describe('Collectable', () => {
   it('collection.getItem() get an item', () => {
     return expect(collection.getItem(2)).to.be.equal(3);
   })
+
+  it('collection.removeItem() remove an item', () => {
+    return expect(collection.removeItem(2)).to.be.equal(undefined);
+  })
   
 });
