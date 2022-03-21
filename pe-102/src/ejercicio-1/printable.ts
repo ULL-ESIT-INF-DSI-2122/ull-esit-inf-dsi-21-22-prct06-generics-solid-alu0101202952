9dd624c3c5b06532a5a6421b974dec0f267dc365
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-21 10:20:18 +0000

    documentacion Printable print() TDD

--- pe-102/src/ejercicio-1/printable.ts
+++ pe-102/src/ejercicio-1/printable.ts
@@ -1,17 +1,31 @@
+/**
+ * EJERCICIO 1 Printable
+ * @interface Printable implementa el método print
+ * @method print() imprime
+ */
 interface Printable<T> {
     print(): void;
 }
 
+/**
+ * @class PrintableClass implementa la interfaz Printable con el método print()
+ * @param recibe el array de valores a imprimir
+ */
 export class PrintableClass<T> implements Printable<T>{
     constructor(private items: T[]) {}
 
-    print() {
+    /**
+     * @method print() imprime el array
+     */
+    public print() {
         this.items.forEach((indice) => {
             return(this.items);
         });
     }
 }
 
-
+/**
+ * Salida por pantalla
+ */
 const printable = new PrintableClass<number>([1, 2, 4]);
 console.log(printable.print());
