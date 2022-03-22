059856498067e07042284113f76880c5033818a3
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-22 21:05:33 +0000

    Documentacion ejercicio1 abstract class Fighter getNombreLuchador() setNombreMundo() y getNombreMundo() TDD

--- practica6-clasesgenericas/src/ejercicio-1/fighter.ts
+++ practica6-clasesgenericas/src/ejercicio-1/fighter.ts
@@ -1,14 +1,33 @@
+type MundoType = 'Pokemon' | 'Marvel' | 'KimetsuNoYaia' | 'MarioBros'
+/**
+ * EJERCICIO 1: EL COMBATE DEFINITIVO
+ * @abstractclass Fighter permite que un contendiente pueda luchar
+ * @param nombre del luchador
+ * @param mundo del que pertenece el luchador
+ */
 export abstract class Fighter {
     constructor(private readonly nombre: string, private mundo: string) {}
 
+    /**
+     * Getter del nombre del luchador
+     * @returns nombre del luchador
+     */
     public getNombreLuchador(){
         return this.nombre;
     }
 
+    /**
+     * Setter del nombre del mundo
+     * @param nombre_mundo al que pertenece un luchador
+     */
     public setNombreMundo(nombre_mundo: string){
         this.mundo = nombre_mundo;
     }
 
+    /**
+     * Getter nombre del mundo
+     * @returns nombre del mundo al que pertenece un luchador
+     */
     public getNombreMundo(){
         return this.mundo;
     }
