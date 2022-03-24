be7a7a54ba4974fd1bbea47d05e39eca18b28ed4
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-24 16:32:10 +0000

    Documentacion y cubrimiento ejercicio 1 class Kimetsu no Yaiba getNombre() TDD

--- practica6-clasesgenericas/src/ejercicio-1/kimetsu.ts
+++ practica6-clasesgenericas/src/ejercicio-1/kimetsu.ts
@@ -1,6 +1,13 @@
 import { Fighter } from "./fighter";
 
-
+/**
+ * EJERCICIO 1.
+ * @class KimetsuNoYaiba extiende de la clase abstracta Fighter, tiene las especificaciones correspondientes 
+ * a los personajes del mundo anime de Kimetsu no Yaiba (cazadores de demonios)
+ * @param nombre tiene el nombre del cazador de demonios (string)
+ * @param respiracion es el poder que domina el cazador de demonios (string) que lleva una katana
+ * @param estadisticas tiene un array del ataque, defensa, velocidad y daño máximo del personaje de Kimetsu no Yaiba (number [])
+ */
 export class KimetsuNoYaiba extends Fighter{
     protected estadisticas = {
         ataque: 0,
@@ -17,6 +24,10 @@ export class KimetsuNoYaiba extends Fighter{
         this.estadisticas.hp = estadisticas[3];
     }
 
+    /**
+     * Getter del nombre del cazador de demonios
+     * @returns nombre del cazador de demonios
+     */
     public getNombre(){
         return this.nombre;
     }
