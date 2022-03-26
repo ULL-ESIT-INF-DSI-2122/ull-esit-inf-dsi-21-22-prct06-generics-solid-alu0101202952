d7a1b7dd3711824e3220dc662a847886acf8a503
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-26 09:56:37 +0000

    Codigo start Class Combat TDD

--- practica6-clasesgenericas/src/ejercicio-1/combat.ts
+++ practica6-clasesgenericas/src/ejercicio-1/combat.ts
@@ -8,8 +8,10 @@ import { Pokemon } from "./pokemon";
  *  (rival)
  */
 export class Combat {
-    constructor(private contendiente1: Fighter, private contendiente2: Fighter){
+    constructor(private contendiente1: Fighter, private contendiente2: Fighter, private catching_phrase: Fighter){
     }
+
+    
 
     /**
      * Método público que inicializa el combate entre luchadores del mismo y distinto mundo
@@ -18,12 +20,16 @@ export class Combat {
     public start(){
         let efectividad1: number = 1;
         let efectividad2: number = 1;
-        let daño_pokemon1: number = 0;
-        let daño_pokemon2: number = 0;
+        let daño1: number = 0;
+        let daño2: number = 0;
+
+        let ronda: number = 0;
         console.log('\nInicio del combate!!\n');
 
         if(this.contendiente1.getVelocidad() > this.contendiente2.getVelocidad()){
             while (this.contendiente1.getHP() > 0 || this.contendiente2.getHP() > 0){
+                console.log('Luchadores: '+this.contendiente1+'contra... '+this.contendiente2);
+                console.log('Ronda '+ronda);
                 if(this.contendiente1 instanceof Pokemon && this.contendiente2 instanceof Pokemon){
                     if (this.contendiente1.getTipo() == "Fuego" && this.contendiente2.getTipo() == "Hierba" || this.contendiente1.getTipo() == "Agua" && this.contendiente2.getTipo() == "Fuego" || this.contendiente1.getTipo() == "Hierba" && this.contendiente2.getTipo() == "Agua" || this.contendiente1.getTipo() == "Eléctrico" && this.contendiente2.getTipo() == "Agua") {
                         efectividad1 = efectividad1 * 2;
@@ -38,26 +44,27 @@ export class Combat {
                         efectividad1 = efectividad1 * 0.5;
                         efectividad2 = efectividad2 * 0.5;
                     }
+
+                    daño1 = 50 * (this.contendiente1.getAtaque() / this.contendiente2.getDefensa()) * efectividad1;
+                    daño1 = Math.round(daño1);
+                    daño2 = 50 * (this.contendiente2.getAtaque() / this.contendiente1.getDefensa()) * efectividad2;
+                    daño2 = Math.round(daño2);
+        
+                    console.log('El tipo de tu pokemon es: '+this.contendiente1.getTipo()+'\nContra un pokemon oponente del tipo: '+this.contendiente2.getTipo()+'\nTu ataque es: '+this.contendiente1.getAtaque()+'\nContra la defensa de tu rival: '+this.contendiente2.getDefensa());
+
+                } else { //Son de otros mundos o mismo mundo pero NO mismo mundo Pokemon, se rige por quien ataca antes
+                    daño1 = 50 * (this.contendiente1.getAtaque() / this.contendiente2.getDefensa()) * efectividad1;
+                    daño1 = Math.round(daño1);
+                    daño2 = 50 * (this.contendiente2.getAtaque() / this.contendiente1.getDefensa()) * efectividad2;
+                    daño2 = Math.round(daño2);
+                    
+                    console.log('El personaje: '+this.contendiente1.getNombreLuchador()+'\nContra su oponente: '+this.contendiente2.getNombreLuchador()+'\nTu ataque es: '+this.contendiente1.getAtaque()+'\nContra la defensa de tu rival: '+this.contendiente2.getDefensa());
                 }
-                
+                ronda++;
             }
             
         }
-        
-
-   
-        daño_pokemon1 = 50 * (this.pokemon1.getAtaque() / this.pokemon2.getDefensa()) * efectividad1;
-        daño_pokemon1 = Math.round(daño_pokemon1);
-        daño_pokemon2 = 50 * (this.pokemon2.getAtaque() / this.pokemon1.getDefensa()) * efectividad2;
-        daño_pokemon2 = Math.round(daño_pokemon2);
-        
-        console.log('El tipo de tu pokemon es: '+this.pokemon1.getTipo()+'\nContra un pokemon oponente del tipo: '+this.pokemon2.getTipo()+'\nTu ataque es: '+this.pokemon1.getAtaque()+'\nContra la defensa de tu rival: '+this.pokemon2.getDefensa());
-
-        console.log('\nTu pokemon causa un daño de '+daño_pokemon1);
-
-        console.log('\nAhora es el turno del pokemon rival, este causa un daño de '+daño_pokemon2+' a tu pokemon');
-
-        return daño_pokemon1;
+        return daño1;
     }
 }
 
