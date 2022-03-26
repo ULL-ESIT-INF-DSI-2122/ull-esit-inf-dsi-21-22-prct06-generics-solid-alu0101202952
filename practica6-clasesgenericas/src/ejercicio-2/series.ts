ea5b3cf5ff0056cae994f799e97e93f336a33cd9
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-26 17:56:18 +0000

    Documentacion ejercicio 2 class Series getNombre() TDD

--- practica6-clasesgenericas/src/ejercicio-2/series.ts
+++ practica6-clasesgenericas/src/ejercicio-2/series.ts
@@ -1,7 +1,18 @@
-
+/**
+ * EJERCICIO 2 DSIFlix
+ * @class Series contiene las series de la plataforma
+ * @param nombre de la serie
+ * @param autor de la serie
+ * @param generos de la serie array de strings
+ * @param anio de publicación
+ */
 export class Series{
     constructor(protected nombre: string, protected autor: string, protected generos: string[] = [], protected anio: number){}
 
+    /**
+     * Getter del nombre de la serie
+     * @returns 
+     */
     public getNombre(){
         return this.nombre;
     }
