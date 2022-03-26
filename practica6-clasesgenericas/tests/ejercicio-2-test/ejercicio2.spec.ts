8cbc6b08555210f5f0516a82db1f5bf5d36c8caa
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-26 19:39:28 +0000

    Test ejercicio 2 class Documentales getNombre() getDirector() getGenero() getAnio() TDD

--- practica6-clasesgenericas/tests/ejercicio-2-test/ejercicio2.spec.ts
+++ practica6-clasesgenericas/tests/ejercicio-2-test/ejercicio2.spec.ts
@@ -2,6 +2,7 @@ import 'mocha';
 import {expect} from 'chai';
 import {Series} from '../../src/ejercicio-2/series';
 import {Peliculas} from '../../src/ejercicio-2/peliculas';
+import {Documentales} from '../../src/ejercicio-2/documentales';
 
 let serie1 = new Series('Juego de Tronos', 'George R. R. Martin', ['Aventura', 'Drama', 'Fantasía', 'Medieval'], 1996);
 let serie2 = new Series('Vikingos', 'Michael Hirst', ['Histórico', 'Drama', 'Fantasía', 'Acción'], 2013);
@@ -10,6 +11,9 @@ let serie3 = new Series('Fugitiva', 'Joaquín Oristrell', ['Thriller'], 2018);
 let pelicula1 = new Peliculas('Titanic', 'James Cameron', ['Kate Winslet', 'Leonardo DiCaprio'], ['Romance', 'Drama', 'Histórico'], 1997);
 let pelicula2 = new Peliculas('La fuerza del cariño', 'James L. Brooks', ['Shirley MacLaine', 'Debra Winger', 'Jack Nicholson'], ['Drama', 'Comedia'], 1983);
 let pelicula3 = new Peliculas('Asesinato en el Orient Express', 'Kenneth Branagh', ['Kenneth Branagh'], ['Policiaca', 'Misterio'], 2017);
+
+let documental1 = new Documentales('Los zorros', 'National Geographic España', ['Naturaleza'], 2017);
+let documental2 = new Documentales('Viaje al centro de la tele', 'Pedro Santos Movellán', ['Música', 2013]);
 
 describe('Series', () => {
   it('serie1.getNombre() returns nombre de la serie', () => {
@@ -41,5 +45,20 @@ describe('Películas', () => {
   })
   it('pelicula2.getAnio() returns año de la pelicula', () => {
     return expect(pelicula2.getAnio()).to.be.equal(1983);
+  })
+})
+
+describe('Documentales', () => {
+  it('documental1.getNombre() returns nombre del documental', () => {
+    return expect(documental1.getNombre()).to.be.equal('Los zorros');
+  })
+  it('documental1.getDirector() returns nombre del director del documental', () => {
+    return expect(documental1.getDirector()).to.be.equal('National Geographic España');
+  })
+  it('documental1.getGenero() returns género/s del documental', () => {
+    return expect(documental1.getGenero()).to.be.eql(['Naturaleza']);
+  })
+  it('documental1.getAnio() returns año del documental', () => {
+    return expect(documental1.getAnio()).to.be.equal(2017);
   })
 })
