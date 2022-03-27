46888ceb27f2c97d3e0d0aab4a76c3455a4caa22
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-27 10:30:23 +0000

    Test ejercicio 3 class Mensaje getNumeroCaracteres() TDD

--- practica6-clasesgenericas/src/ejercicio-3/mensaje.ts
+++ practica6-clasesgenericas/src/ejercicio-3/mensaje.ts
@@ -1,6 +1,15 @@
+/**
+ * EJERCICIO 3: CIFRADO CÉSAR
+ * @class Mensaje contiene las características del mensaje
+ * @param mensaje que se va a tratar
+ */
 export class Mensaje{
     constructor(protected mensaje: string){}
 
+    /**
+     * Getter del mensaje a cifrar
+     * @returns mensaje
+     */
     public getMensaje(){
         return this.mensaje;
     }
--- practica6-clasesgenericas/tests/ejercicio-3-test/ejercicio3.spec.ts
+++ practica6-clasesgenericas/tests/ejercicio-3-test/ejercicio3.spec.ts
@@ -8,4 +8,8 @@ let mensaje1 = new Mensaje('HOLAESTOESUNAPRUEBA');
 describe('Mensaje', () => {
   it('mensaje1.getMensaje() returns mensaje a encriptar', () => {
     return expect(mensaje1.getMensaje()).to.be.equal('HOLAESTOESUNAPRUEBA');
+  })
+  it('mensaje1.getNumeroCaracteres() returns tamaño del mensaje', () => {
+    return expect(mensaje1.getNumeroCaracteres()).to.be.equal(10);
+  })
 })
