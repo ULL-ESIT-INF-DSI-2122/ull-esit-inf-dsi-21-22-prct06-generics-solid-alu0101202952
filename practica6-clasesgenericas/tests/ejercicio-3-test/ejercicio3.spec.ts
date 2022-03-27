62846f40e075e91aa06ca40aeb7964ad9c021d5d
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-27 10:48:10 +0000

    Test ejercicio 3 class CifradoCesar getMensaje() getClave() TDD

--- practica6-clasesgenericas/tests/ejercicio-3-test/ejercicio3.spec.ts
+++ practica6-clasesgenericas/tests/ejercicio-3-test/ejercicio3.spec.ts
@@ -2,10 +2,12 @@ import 'mocha';
 import {expect} from 'chai';
 import {Mensaje} from '../../src/ejercicio-3/mensaje';
 import {Clave} from '../../src/ejercicio-3/clave';
+import {CifradoCesar} from '../../src/ejercicio-3/cifradocesar';
 
 
 let mensaje1 = new Mensaje('HOLAESTOESUNAPRUEBA');
 let clave = new Clave('CLAVE');
+let cifradocesar1 = new CifradoCesar(mensaje1, clave);
 
 describe('Mensaje', () => {
   it('mensaje1.getMensaje() returns mensaje a encriptar', () => {
@@ -22,5 +24,14 @@ describe('Clave', () => {
   })
   it('clave.getNumeroCaracteres() returns tamaño de la clave', () => {
     return expect(clave.getNumeroCaracteres()).to.be.equal(5);
+  })
+})
+
+describe('Cifrado César', () => {
+  it('cifradocesar1.getCifrado() returns mensaje a encriptar', () => {
+    return expect(cifradocesar1.getMensaje()).to.be.equal('HOLAESTOESUNAPRUEBA');
+  })
+  it('cifradocesar1.getClave() returns clave con la que encriptar', () => {
+    return expect(cifradocesar1.getClave()).to.be.equal('CLAVE');
   })
 })
