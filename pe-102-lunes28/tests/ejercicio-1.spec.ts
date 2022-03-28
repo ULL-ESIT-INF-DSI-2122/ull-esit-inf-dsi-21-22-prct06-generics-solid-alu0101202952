ac0b0793591df9a0c43379841072b983ae87272f
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-28 08:43:00 +0000

    Test PE LUNES28 PrimeNumber class getPrimeInstance() TDD

--- pe-102-lunes28/tests/ejercicio-1.spec.ts
+++ pe-102-lunes28/tests/ejercicio-1.spec.ts
@@ -5,6 +5,9 @@ import {PrimeNumber} from '../src/primenumber';
 let primenumber = new PrimeNumber([130, 60, 65, 65]);
 
 describe('Prime Number, com', () => {
+  it('primenumber.getPrimeInstance() returns la instancia', () => {
+    return expect(primenumber.getPrimeInstance()).to.be.equal(primenumber);
+  })
   it('primenumber.getPrimeNumber() returns los números primos', () => {
     return expect(primenumber.getPrimeNumber()).to.be.equal(undefined);
   })
