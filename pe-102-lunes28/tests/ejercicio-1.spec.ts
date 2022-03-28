c9509953f511a1d520076b64bbed7ef33aa736fd
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-28 09:47:08 +0000

    Cubrimiento corregido PE LUNES28 PrimeNumber class getNToMPrime() TDD

--- pe-102-lunes28/tests/ejercicio-1.spec.ts
+++ pe-102-lunes28/tests/ejercicio-1.spec.ts
@@ -9,10 +9,10 @@ describe('Prime Number', () => {
     return expect(primenumber).to.be.equal(primenumber);
   })
   it('primenumber.getPrimeNumber() returns los números primos', () => {
-    return expect(primenumber.getPrimeNumber()).to.be.equal([ 2, 3, 5 ]);
+    return expect(primenumber.getPrimeNumber()).to.be.equal([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ]);
   })
   it('primenumber.getPrimeLenght() returns tamaño del array de primos', () => {
-    return expect(primenumber.getPrimeLenght()).to.be.equal(3);
+    return expect(primenumber.getPrimeLenght()).to.be.equal(10);
   })
   it('primenumber.setPrimeNumber() añade un número primo', () => {
     return expect(primenumber.setPrimeNumber([2,3])).to.be.equal(undefined);
